9bdadbd810a8a04acea0cda4dc8065dc24baf08f
TomasNagy <tomi.nagy@outlook.com>
2023-05-14 13:02:10 +0200

    Add no-vowels.c

---
+++ no-vowels.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+	// Check if there is any argument
+	// If not print the usage of the program
+
+
+	//Call replace function and print the result
+}
